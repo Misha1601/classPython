282f858af63b3537e0d990a2baf67bdd723cda41
Mikhail <nikiforov1601@gmail.com>
2022-05-01 06:47:07 +0300

    next

--- builtins.py
+++ builtins.py
@@ -1,8 +1,8 @@
 """изучение объекта string """
 print('посмотреть все методы:')
 k = 0
-for i in dir(__builtins__):
-    if i.isalpha():
+for en,i in enumerate(dir(__builtins__)):
+    if i.isalpha() and i.endswith('Error')==0:
         print('-------------------------------------------------------------------')
         print(k, i)
         k += 1
--- writeHelpInFile.py
+++ writeHelpInFile.py
@@ -1,7 +1,7 @@
 """изучение объекта string """
 import sys
 
-print('посмотреть справку сласса:')
+print('посмотреть справку класса:')
 f = open('info.txt', 'w')
 k = 0
 sys.stdout = f
